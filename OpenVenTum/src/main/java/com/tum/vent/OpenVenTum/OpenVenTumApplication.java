fe9d341f899569d9924adb9855ca8c16c40e23bc
unknown <ge36qij@mytum.de>
2020-04-17 14:52:39 +0300

    initial

---
+++ OpenVenTum/src/main/java/com/tum/vent/OpenVenTum/OpenVenTumApplication.java
@@ -0,0 +1,13 @@
+package com.tum.vent.OpenVenTum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenVenTumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenVenTumApplication.class, args);
+	}
+
+}
