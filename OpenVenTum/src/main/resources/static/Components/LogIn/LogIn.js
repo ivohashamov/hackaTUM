848998986f0b11a3c57bb3fe063bf966c0de21b4
unknown <ge36qij@mytum.de>
2020-04-18 21:13:55 +0300

    mama uuuuuuu

---
+++ OpenVenTum/src/main/resources/static/Components/LogIn/LogIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LogIn.css';
+
+class LogIn extends React.Component {
+  render() {
+    return (
+      <div className="LogIn">
+      </div>
+    )
+  }
+}
+
+export default LogIn;
